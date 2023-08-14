e35733cac18ab132b71491bc625fa26e3661f2b6
sarazalanderi <sarazalanderi@gmail.com>
2023-08-14 14:34:54 +0700

    Add new features

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // <== Will be created later
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  next();
+});
 server.use(router);
 
 server.listen(port);
