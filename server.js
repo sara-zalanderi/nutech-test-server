0504dc8f2dc0bf06055bb37d68247a2a727eeb0b
sarazalanderi <sarazalanderi@gmail.com>
2023-08-14 14:57:43 +0700

    Add new features

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 const allow = "http://localhost:3000";
 
+server.use(cors());
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
