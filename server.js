cb16e954bb2025a05b50bafef3620bdc4f7b9579
sarazalanderi <sarazalanderi@gmail.com>
2023-08-14 15:07:34 +0700

    Add new features

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults({ noCors: true });
 const port = process.env.PORT || 3001;
-const allow = "http://localhost:3000";
+const allow = ["http://localhost:3000"];
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
@@ -15,6 +16,16 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
+var corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  },
+  methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+  allowedHeaders: "accept, content-type",
+};
+server.use(cors(corsOptions));
 server.use(router);
 
 server.listen(port);
